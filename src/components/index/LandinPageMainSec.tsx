d356852ae9757a554b8a82a6ba6161bff94b9e0a
Raj <varasaniraj5@gmail.com>
2023-07-09 21:35:24 +0530

    add features sec

--- src/components/index/LandinPageMainSec.tsx
+++ src/components/index/LandinPageMainSec.tsx
@@ -4,6 +4,8 @@ import { Button, Title, Text } from "@mantine/core";
 import Logo from "@/assets/general/Logo.svg";
 import LogoFlow from "@/assets/general/LogoFlow.svg";
 import HeroCover from "@/assets/home/HeroCover.svg";
+import FeatureImg1 from "@/assets/home/FeatureImg1.svg";
+import FeatureImg2 from "@/assets/home/FeatureImg2.svg";
 
 import styles from "./LandinPageMainSec.module.css";
 import Image from "next/image";
@@ -60,6 +62,52 @@ function LandinPageMainSec() {
           height={341}
           className={styles.heroCover}
         />
+      </div>
+      <div className={styles.features}>
+        <Title
+          align="left"
+          order={2}
+          size={"var(--h1)"}
+          className={styles.featuresTitle}
+        >
+          <Text weight={500} size={"md"} inherit color="primary.2">
+            How
+          </Text>
+          <Text weight={500} size={"md"} inherit>
+            MeloMint
+          </Text>
+          <Text weight={500} size={"md"} inherit color="primary.2">
+            pulls this off?
+          </Text>
+        </Title>
+        <div className={styles.featureCards}>
+          <div className={styles.feature}>
+            <Image src={FeatureImg1} alt="Melomint" width={384} height={384} />
+            <div className={styles.featureText}>
+              <Title align="left" order={3}>
+                Fair Share
+              </Title>
+              <Text color="primary.3">
+                Measures and calculates the duration of each sampled part,
+                guaranteeing that the original artist receives a fair share
+                based on usage
+              </Text>
+            </div>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.featureText}>
+              <Title align="left" order={3}>
+                Masterpiece NFT{" "}
+              </Title>
+              <Text color="primary.3">
+                Measures and calculates the duration of each sampled part,
+                guaranteeing that the original artist receives a fair share
+                based on usage
+              </Text>
+            </div>
+            <Image src={FeatureImg2} alt="Melomint" width={384} height={384} />
+          </div>
+        </div>
       </div>
     </div>
   );
