7f2a215176e15af4250617afa813dab49f661d44
Raj <varasaniraj5@gmail.com>
2023-07-09 22:32:10 +0530

    update header comp

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -8,16 +8,20 @@ import { flowicon, user, search } from "@/assets/player";
 
 function Header() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background py-5 px-[2.25rem] flex justify-between">
+    <header className="sticky top-0 z-40 w-full bg-background py-5 px-[2.25rem] flex justify-between">
       <TextInput
-        classNames={{input:"bg-[#108CFF0D] border-none min-w-[400px] rounded-[0.875rem]"}}
+        classNames={{
+          input: "bg-[#108CFF0D] border-none min-w-[400px] rounded-[0.875rem]",
+        }}
         variant="filled"
         placeholder="Search"
         size="md"
         icon={<Image src={search} alt="" height={20} width={20} />}
       />
       <div className="flex items-center space-x-5">
-        <Text size="lg" weight="700">Raj Varsani</Text>
+        <Text size="lg" weight="700">
+          Raj Varsani
+        </Text>
         <div className="relative">
           <Image
             src={flowicon}
