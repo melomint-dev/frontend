9545524b5bd6607b26b96468d138c779a509094e
Raj <varasaniraj5@gmail.com>
2023-07-10 00:17:11 +0530

    fix header

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -3,16 +3,6 @@ import styles from "./Header.module.css";
 import { TextInput, Text } from "@mantine/core";
 import { flowicon, user, search } from "@/assets/player";
 
-const userImageStyle = {
-  borderRadius: "14px",
-};
-
-const flowiconStyle = {
-  position: "absolute",
-  top: "-0.5rem",
-  left: "-0.5rem",
-};
-
 function Header() {
   return (
     <header className={styles.container}>
@@ -28,7 +18,6 @@ function Header() {
           Raj Varsani
         </Text>
         <div className={styles.userImageWithFlow}>
-          .
           <div className={styles.flowIcon}>
             <Image
               src={flowicon}
@@ -43,7 +32,7 @@ function Header() {
             alt=""
             height={42}
             width={42}
-            style={userImageStyle}
+            className={styles.userImage}
           />
         </div>
       </div>
