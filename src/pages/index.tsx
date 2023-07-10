7535ccfeab46fc2afc591de91c652c47c48d6130
Raj <varasaniraj5@gmail.com>
2023-07-10 22:59:01 +0530

    created modals

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,45 @@
+import AuthComp from "@/components/auth/AuthComp";
+import { Button, Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import LandinPageMainSec from "@/components/index/LandinPageMainSec";
-import { Button } from "@mantine/core";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  console.log(router.query);
+
+  const closeAuthModal = () => {
+    router.push("/");
+  };
+
   return (
     <main>
       <LandinPageMainSec />
       <Button />
+      <Modal
+        opened={router.query.authModal === "register"}
+        onClose={closeAuthModal}
+        withCloseButton={false}
+        styles={{
+          body: {
+            padding: 0,
+          },
+        }}
+      >
+        <AuthComp type="register" />
+      </Modal>
+      <Modal
+        opened={router.query.authModal === "login"}
+        onClose={closeAuthModal}
+        withCloseButton={false}
+        styles={{
+          body: {
+            padding: 0,
+          },
+        }}
+      >
+        <AuthComp type="login" />
+      </Modal>
     </main>
   );
 }
