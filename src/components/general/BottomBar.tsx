26b0a7afbb09b2a4a8413e6d0ae316b4e06fc90a
Malay Patel <101856674+malay44@users.noreply.github.com>
2023-07-10 22:03:31 +0530

    bottombar fixed

--- src/components/general/BottomBar.tsx
+++ src/components/general/BottomBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Image from "next/image";
 import { Title, Text, Slider } from "@mantine/core";
 import {
@@ -9,7 +9,7 @@ import {
   playbutton,
   prevbutton,
   volume,
-} from "@/assets/general"
+} from "@/assets/general";
 
 import styles from "./Bottombar.module.css";
 
@@ -18,24 +18,34 @@ function BottomBar() {
   return (
     <div className={styles.container}>
       <div className={styles.musicName}>
-        <Title order={5} color="primary" weight={700} className={styles.musicFont}>
+        <Text
+          size="lg"
+          color="primary"
+          weight={700}
+          className={styles.musicFont}
+        >
           Living My Best Life
-        </Title>
+        </Text>
         <Text size="sm" color="primary.2" weight={400}>
           Ben Hector
         </Text>
       </div>
       <div className={styles.playBar}>
         <div className={styles.timeLine}>
-          <Text color="primary" weight={500} size="xs">
+          <Text color="primary.1" weight={500} size="xs">
             1.21
           </Text>
-          <Slider color="primary" size="sm" value={value} onChange={setValue}/>
-          <Text color="primary" weight={500} size="xs">
+          <Slider
+            color="primary"
+            size="sm"
+            value={value}
+            onChange={setValue}
+          />
+          <Text color="primary.1" weight={500} size="xs">
             2.36
           </Text>
         </div>
-        <div  className={styles.bar}>
+        <div className={styles.bar}>
           <button className={styles.button}>
             <Image src={backbutton} alt="" />
           </button>
@@ -55,16 +65,13 @@ function BottomBar() {
       </div>
 
       <div className={styles.rightBar}>
-        <div>
-          <Image src={heart} alt="" className="" />
-        </div>
+        <Image src={heart} alt="" className="" />
         <div className={styles.volumeBar}>
           <Image src={volume} alt="" className="" />
-          <Slider color="primary" size="sm"/>
+          <Slider color="primary" size="sm" />
         </div>
       </div>
     </div>
-  
   );
 }
 
