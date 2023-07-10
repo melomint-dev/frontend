2d8ab36033a7727cefc913fb977137e2a044ab05
digant15803 <digantpatel1582003@gmail.com>
2023-07-11 00:07:50 +0530

    side-bar added

--- src/components/general/BottomBar.tsx
+++ src/components/general/BottomBar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
-import { Title, Text, Slider } from "@mantine/core";
+import { Text, Slider } from "@mantine/core";
 import {
   backbutton,
   fastforardbutton,
