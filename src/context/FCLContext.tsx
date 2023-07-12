c5687dd7e823eb578486a668c4c7d38b7c72e0e2
kavania2002 <kavan155gondalia@gmail.com>
2023-07-12 16:49:23 +0530

    Revert "context changed"
    This reverts commit 755cc395cb73dc1ab08fca5a13963bfeb9167e9e.

--- src/context/FCLContext.tsx
+++ src/context/FCLContext.tsx
@@ -5,7 +5,6 @@ import {
   useState,
   useCallback,
   useMemo,
-  useEffect,
 } from "react";
 
 import type { ReactNode } from "react";
@@ -43,10 +42,6 @@ export default function FclContextProvider({
       console.log(error);
     }
   };
-
-  useEffect(() => {
-    console.log("currentUser", currentUser);
-  }, [currentUser]);
 
   const logout = useCallback(async () => {
     await fcl.unauthenticate();
