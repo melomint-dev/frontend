61af517da5cc7a34e4719c253ed234c7161ea5fb
kavania2002 <kavan155gondalia@gmail.com>
2023-07-13 00:04:48 +0530

    update: create song

---
+++ src/cadence/transactions/addSong.ts
@@ -0,0 +1,16 @@
+export const addSongTransaction = `
+import MeloMint from 0xMeloMint
+
+transaction(name: String, img: String, url: String) {
+  prepare(signer: AuthAccount) {
+    let creatorId = MeloMint.getCreatorIdByAddress(addr: signer.address)
+    MeloMint.createSong(name: name, creator: signer.address, img: img, url: url, creatorId: creatorId)
+  }
+
+  execute {
+    log(MeloMint.getSongs())
+    log(MeloMint.getCreators())
+  }
+}
+
+`
