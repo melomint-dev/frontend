9a5f9c5b37dc0c10ad4f5992a7be1bc2995f4660
digant15803 <digantpatel1582003@gmail.com>
2023-07-14 20:17:08 +0530

    audio added to context

--- src/components/general/BottomBar.tsx
+++ src/components/general/BottomBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import Image from "next/image";
 import { Text, Slider } from "@mantine/core";
 import {
@@ -11,6 +11,7 @@ import {
   volume,
   pausebutton,
 } from "@/assets/general";
+import { MusicContext } from "@/context/MusicContext";
 
 import styles from "./Bottombar.module.css";
 
@@ -22,93 +23,27 @@ function MusicControllor({ image, func }: { image: string; func:()=> void }) {
   );
 };
 
-const BottomBar = ({ audioUrl }: {audioUrl: string}) => {
+const BottomBar = () => {
   interface TimeObject {
     [key: string]: number;
   }
 
-  const [audio, setAudio] = useState<HTMLAudioElement>();
-  const [currentTime, setCurrentTime] = useState(0);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [duration, setDuration] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [volumeValue, setVolume] = useState(100);
-  const [seekTime, setSeekTime] = useState(0);
+  const { audio, currentTime, duration, isPlaying, setIsPlaying, volumeValue, setVolume, setSeekTime } = useContext(MusicContext);
 
-  useEffect(() => {
-    if(audio){
-      audio!.volume = volumeValue / 100;
-    }
-  }, [volumeValue]);
+  const forward = () => {
+      setSeekTime(audio!.currentTime + 10);
+  }
 
-  useEffect(() => {
-    if(audio){
-      audio.currentTime = seekTime;
-      setCurrentTime(seekTime);
-    }
-  }, [seekTime]);
-
-  useEffect(() => {
-    const fetchAudio = async () => {
-      const response = await fetch(audioUrl);
-      const audioBlob = await response.blob();
-      const audioObjectURL = URL.createObjectURL(audioBlob);
-      setAudio(new Audio(audioObjectURL));
-    };
-
-    fetchAudio();
-
-    return () => {
-      if (audio) {
-        audio.pause();
-        URL.revokeObjectURL(audio.src);
-      }
-    };
-  }, [audioUrl]);
-  
-  useEffect(() => {
-    const handleTimeUpdate = () => {
-      if(audio){
-        setCurrentTime(audio.currentTime);
-      }
-    };
-
-    const handleLoadedData = () => {
-      setIsLoaded(true);
-      if(audio){
-        setDuration(audio.duration);
-      }
-    };
-
-    const handlePlay = () => {
-      setIsPlaying(true);
-    };
-
-    const handlePause = () => {
-      setIsPlaying(false);
-    };
-
-    if (audio) {
-      audio.addEventListener("timeupdate", handleTimeUpdate);
-      audio.addEventListener("loadeddata", handleLoadedData);
-      audio.addEventListener("play", handlePlay);
-      audio.addEventListener("pause", handlePause);
-    }
-
-    return () => {
-      if (audio) {
-        audio.removeEventListener("timeupdate", handleTimeUpdate);
-        audio.removeEventListener("loadeddata", handleLoadedData);
-        audio.removeEventListener("play", handlePlay);
-        audio.removeEventListener("pause", handlePause);
-      }
-    };
-  }, [audio]);
+  const backward = () => {
+      setSeekTime(audio!.currentTime - 10);
+  }
 
   const handlePlayPause = () => {
     if (isPlaying && audio) {
+      setIsPlaying(false);
       audio.pause();
     } else if(audio) {
+      setIsPlaying(true);
       audio.play();
     }
   };
@@ -141,16 +76,16 @@ const BottomBar = ({ audioUrl }: {audioUrl: string}) => {
           <Text color="primary.1" weight={500} fz="xs" className={styles.timeText}>
             {formatTime({timeInSeconds: currentTime})}
           </Text>
-          <Slider color="primary" size="sm" value={currentTime} onChange={setSeekTime} min={0} max={duration} label={null}/>
+          <Slider color="primary" size="sm" value={currentTime} step={0.01} onChange={setSeekTime} min={0} max={duration} label={null}/>
           <Text color="primary.1" weight={500} fz="xs" className={styles.timeText}>
             {formatTime({timeInSeconds: duration})}
           </Text>
         </div>
         <div className={styles.bar}>
           <MusicControllor image={backbutton} func={handlePlayPause}/>
-          <MusicControllor image={prevbutton} func={handlePlayPause}/>
+          <MusicControllor image={prevbutton} func={backward}/>
           {isPlaying ? <MusicControllor image={pausebutton} func={handlePlayPause}/> : <MusicControllor image={playbutton} func={handlePlayPause}/>}
-          <MusicControllor image={nextbutton} func={handlePlayPause}/>
+          <MusicControllor image={nextbutton} func={forward}/>
           <MusicControllor image={fastforardbutton} func={handlePlayPause}/>
         </div>
       </div>
--- src/components/general/Layout.tsx
+++ src/components/general/Layout.tsx
@@ -1,13 +1,18 @@
-import {useState} from "react";
+import {useContext} from "react";
 
 import Sidebar from "@/components/general/Sidebar";
 import Header from "@/components/general/Header";
 import BottomBar from "@/components/general/BottomBar";
+import { MusicContext } from "@/context/MusicContext";
 
 import styles from "@/components/general/Layout.module.css";
 
 function Layout({ childern }: { childern: React.ReactNode }) {
-
+  const { audioURL, setAudioUrl } = useContext(MusicContext);
+  if(audioURL !== "https://melomint.centralindia.cloudapp.azure.com/api/get-file/QmdztfvDRgVaUUs5SoHM4HNnsy8t9A1xmtN1k8Ky9XYC8r"){
+    setAudioUrl("https://melomint.centralindia.cloudapp.azure.com/api/get-file/QmdztfvDRgVaUUs5SoHM4HNnsy8t9A1xmtN1k8Ky9XYC8r");
+  }
+  
   // const [ipfsHash, setIpfsHash] = useState("");
   // const [selectedFile, setSelectedFile] = useState(null);
   // const audioUrl = `http://localhost:3000/get-file/${ipfsHash}`;
@@ -43,7 +48,7 @@ function Layout({ childern }: { childern: React.ReactNode }) {
           {childern}
         </div>
       </div>
-      <BottomBar audioUrl="https://melomint.centralindia.cloudapp.azure.com/api/get-file/QmdztfvDRgVaUUs5SoHM4HNnsy8t9A1xmtN1k8Ky9XYC8r"/>
+      <BottomBar />
     </div>
   );
 }
