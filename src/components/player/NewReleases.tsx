00f836056499df38d613bbd2a2c9d2d903762726
digant15803 <digantpatel1582003@gmail.com>
2023-07-17 13:39:01 +0530

    Onclick Layout change integrated for New Song Component

--- src/components/player/NewReleases.tsx
+++ src/components/player/NewReleases.tsx
@@ -3,37 +3,24 @@ import { Title, Text } from "@mantine/core";
 import NewSongComponents from "./NewSongComponents";
 import { user } from "@/assets/player";
 
+const TEMP_SONGS_DATA = new Array(4).fill({}).map((_, i) => ({
+  _id: i.toString(),
+  name: "Song Name",
+  artist: "Jigardan Gadhvi",
+  image: "https://picsum.photos/300/300?random=" + i,
+}));
+
 const NewReleases = () => {
+  const songs = TEMP_SONGS_DATA;
   return (
     <div className={styles.container}>
       <Title order={5} weight={700}>
         New Releases
       </Title>
       <div className={styles.songs}>
-        <NewSongComponents 
-          song={{
-            _id: "1",
-            name: "Song Name",
-            image: user,
-            artist: "Artist Name",
-          }}
-        />
-        <NewSongComponents 
-          song={{
-            _id: "2",
-            name: "Song Name",
-            image: user,
-            artist: "Artist Name",
-          }}
-        />
-        <NewSongComponents 
-          song={{
-            _id: "3",
-            name: "Song Name",
-            image: user,
-            artist: "Artist Name",
-          }}
-        />
+      {songs.map((song) => (
+          <NewSongComponents song={song} key={song._id} />
+        ))}
       </div>
     </div>
   );
--- src/components/player/NewSongComponents.tsx
+++ src/components/player/NewSongComponents.tsx
@@ -1,31 +1,48 @@
+import {useContext} from "react";
 import styles from "./NewSongComponents.module.css";
 
 import Image, { StaticImageData } from "next/image";
 import { Text } from "@mantine/core";
+import { MusicContext } from "@/context/MusicContext";
 
 const NewSongComponents = ({
-  song: { name, image, artist },
+  song,
 }: {
   song: {
     _id: string;
     name: string;
-    image: StaticImageData | string;
+    image: string;
     artist: string;
   };
 }) => {
+  interface HashObject {
+    [key: string]: string;
+  }
+  const { audioURL, setAudioUrl, setArtistName, setCoverPhotoSrc, setMusicName } = useContext(MusicContext);
+  const changeSong = (ipfsHash:HashObject) => {
+    const cURL = "https://melomint-infra.centralindia.cloudapp.azure.com/api/get-file/";
+    const { hash } = ipfsHash;
+    console.log(hash);
+    if(audioURL !== (cURL+hash)){
+      setAudioUrl(cURL+hash);
+      setMusicName(song.name);
+      setArtistName(song.artist);
+      setCoverPhotoSrc(song.image);
+    }
+  } 
   return (
-    <div className={styles.container}>
-      <Image src={image} alt="" className={styles.songImage} />
+    <div className={styles.container} onClick={() => changeSong({hash: "QmdztfvDRgVaUUs5SoHM4HNnsy8t9A1xmtN1k8Ky9XYC8r"})}>
+      <Image src={song.image} alt="" className={styles.songImage} width={300} height={300}/>
       <Text
         weight={700}
         color="primary"
         lineClamp={1}
         className={styles.songName}
       >
-        {name}
+        {song.name}
       </Text>
       <Text weight={700} color="primary.3" className={styles.songArtistName}>
-        {artist}
+        {song.artist}
       </Text>
     </div>
   );
