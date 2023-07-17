88e5a2b08a617d2b3568da1e0735d8c8053ae14d
Raj <varasaniraj5@gmail.com>
2023-07-18 00:50:34 +0530

    remove temp data

--- src/pages/player/favourites.tsx
+++ src/pages/player/favourites.tsx
@@ -4,14 +4,6 @@ import FavouritesTopSec from "@/components/player/FavouritesMainSec";
 import { useUser } from "@/hooks/person.swr";
 import { useSongList } from "@/hooks/song.swr";
 
-const TEMP_SONGS_DATA = new Array(10).fill({}).map((_, i) => ({
-  _id: i.toString(),
-  name: "Song Name",
-  artist: "Jigardan Gadhvi",
-  image: "https://picsum.photos/300/300?random=" + i,
-  duration: 150,
-}));
-
 function ArtistProfile() {
   const { userData, isUserDataLoading, errorFetchingUserData } = useUser();
   const { songListData, isSongListDataLoading, errorFetchingSongListData } =
