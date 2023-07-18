2f61a1517e5e769080312335b3b478a295c3395b
Raj <varasaniraj5@gmail.com>
2023-07-18 13:04:36 +0530

    🛠️ fix imports

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -2,7 +2,7 @@ import * as fcl from "@onflow/fcl";
 
 import {
   addSongTransaction,
-  updateNFTPriceTransaction,
+  // updateNFTPriceTransaction,
   buyNFTTransaction,
 } from "@/cadence/transactions";
 import { singleUserTransaction } from "@/utils/transcation";
@@ -77,18 +77,18 @@ class TransactionService {
     }
   };
 
-  updateNFTPrice = async ({ price }: { price: number }) => {
-    try {
-      await singleUserTransaction({
-        code: updateNFTPriceTransaction,
-        args: [fcl.arg(price.toString(), fcl.t.UInt)],
-      });
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  };
+  // updateNFTPrice = async ({ price }: { price: number }) => {
+  //   try {
+  //     await singleUserTransaction({
+  //       code: updateNFTPriceTransaction,
+  //       args: [fcl.arg(price.toString(), fcl.t.UInt)],
+  //     });
+  //     return true;
+  //   } catch (error) {
+  //     console.log(error);
+  //     return false;
+  //   }
+  // };
 
   buyNFT = async ({ price }: { price: number }) => {
     try {
